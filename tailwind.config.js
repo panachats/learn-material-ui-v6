0bf1f7b03066a7b76f0a73ffabbe9168da84de2f
panachats <143708225+panachats@users.noreply.github.com>
2024-12-04 18:05:45 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [
+    require("./src/mui-treasury/tailwind-preset/material-ui-tailwind-preset"),
+  ],
+}
+
